d2d20c64175deb3e2d899fb76802451d274021bc
M. Yilmaz SUSLU <yilmazsuslu@gmail.com>
2025-11-27 00:46:57 +0100

    Cleanup .gitignore, remove target/ from git history (already ignored)

--- target/classes/db/migration/V3__Add_location_columns_to_businesses.sql
+++
@@ -1,6 +0,0 @@
--- V3__Add_location_columns_to_businesses.sql
--- Add missing latitude and longitude columns to the businesses table
-
-ALTER TABLE businesses
-ADD COLUMN latitude FLOAT,
-ADD COLUMN longitude FLOAT;
