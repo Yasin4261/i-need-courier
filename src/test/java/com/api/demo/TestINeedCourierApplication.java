af4af7e0b2fb910f9af2c16c47e06f1a3be94c28
M. Yilmaz SUSLU <yilmazsuslu@gmail.com>
2025-12-07 14:09:28 +0100

    Rename TestConfiguration, drop BusinessAuthController

--- src/test/java/com/api/demo/TestINeedCourierApplication.java
+++ src/test/java/com/api/demo/TestINeedCourierApplication.java
@@ -1,13 +1,13 @@
 package com.api.demo;
 
-import com.api.demo.config.container.TestContainersConfiguration;
+import com.api.demo.config.container.TestConfiguration;
 import org.springframework.boot.SpringApplication;
 
 public class TestINeedCourierApplication {
 
         public static void main(String[] args) {
             SpringApplication.from(INeedCourierApplication::main)
-                    .with(TestContainersConfiguration.class)
+                    .with(TestConfiguration.class)
                     .run(args);
         }
 
