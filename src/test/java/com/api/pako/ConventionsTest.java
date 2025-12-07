6c70867903cc8499ec1214d8a8347f23d089aed9
M. Yilmaz SUSLU <yilmazsuslu@gmail.com>
2025-12-07 15:35:56 +0100

    New consistency test: noAutowiredUseInSourceCodeInFavorOfConstructorInjection

--- src/test/java/com/api/pako/ConventionsTest.java
+++ src/test/java/com/api/pako/ConventionsTest.java
@@ -9,6 +9,7 @@ import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import lombok.Generated;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 // Ensures architectural conventions are followed
 class ConventionsTest {
@@ -29,6 +30,15 @@ class ConventionsTest {
                 .or().haveRawType(java.util.logging.Logger.class)
                 .should().beAnnotatedWith(Generated.class)
                 .because("Logging must be done through @Sl4j annotation that Lombok generates");
+        rule.check(SOURCES);
+    }
+
+    @Test
+    @ArchTest
+    void noAutowiredUseInSourceCodeInFavorOfConstructorInjection() {
+        ArchRule rule = ArchRuleDefinition.noFields()
+                .should().beAnnotatedWith(Autowired.class)
+                .because("Prefer constructor injection over field injection via Autowired");
         rule.check(SOURCES);
     }
 }
